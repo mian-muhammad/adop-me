ae9a8191af8963ca3727ecc080aac5e12163207a
Mian Muhammad <mianmuhammad80@gmail.com>
2022-09-04 19:53:59 +0500

    component composition

---
+++ src/Result.js
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+function Result({ pets }) {
+  return (
+    <div>
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+            id={pet.id}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Result;
