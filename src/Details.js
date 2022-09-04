f3f6d02f9208f5a653edb0553a363ff77921ad59
Mian Muhammad <mianmuhammad80@gmail.com>
2022-09-05 02:59:21 +0500

    class components

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,45 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-function Details() {
-  const { id } = useParams();
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-  return <div>Details {id}</div>;
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
