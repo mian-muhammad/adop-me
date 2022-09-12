ee36b7407c3abe9ba0bbb4d77066e18b8ef6dc4e
Mian Muhammad <mianmuhammad80@gmail.com>
2022-09-12 23:35:02 +0500

    code splitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
+import { StrictMode, useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
-import { StrictMode, useState } from "react";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
 
   return (
     <StrictMode>
-      <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/" element={<SearchParams />} />
-            <Route path="/details/:id" element={<Details />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeContext.Provider>
+      <Suspense fallback=<h2>Loading, be patient.</h2>>
+        <ThemeContext.Provider value={theme}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/" element={<SearchParams />} />
+              <Route path="/details/:id" element={<Details />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeContext.Provider>
+      </Suspense>
     </StrictMode>
   );
 };
--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,10 @@
-import { Component } from "react";
+import { Component, lazy } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
-import Modal from "./Modal";
 import ThemeContext from "./ThemeContext";
+
+const Modal = lazy(() => import("./Modal"));
 
 class Details extends Component {
   state = { loading: true, showModal: false };
